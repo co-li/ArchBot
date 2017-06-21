9dd49b316e3b887a4301c609b2a1638fb97d952b
kingofinfo <kingofinfo@users.noreply.github.com>
2017-06-20 23:22:19 -0700

    Created file for the bot modified:   README.md new file:   bot.py

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# ArchBot
+## ArchBot
 A bot for searching the ArchWiki, Arch Linux packages, and AUR
+Currently, the only way to change the bot prefix is by editing `bot.py`
+
+# Requirements
+**ArchBot** requires:
+- [Python 3.5 or newer](https://www.python.org/)
+- [Discord.py](https://github.com/Rapptz/discord.py)
+
+## Commands
+```
+ArchBot commands:
+	repos <package>		Search for a package from the Arch Linux repositories
+	aur <package>		Search for a package from the Arch User Repository
+	wiki <package>		Search for an article from the ArchWiki
+	source			Get the link to the bot's source code
+	uptime			Check the uptime of the bot
+```
---
+++ bot.py
@@ -0,0 +1,54 @@
+# Import what's needed by the bot
+import discord
+from discord.ext import commands
+import asyncio
+import json
+import datetime
+import logging
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+
+description = '''ArchBot is a Discord bot written in Python for searching the ArchWiki, Arch Linux package repositories, and AUR'''
+
+# Set bot token
+token = 'Your bot token'
+
+# Set bot prefix
+prefix = 'sudo '
+
+# Create bot
+bot = commands.Bot(command_prefix=prefix, description=description)
+
+# Create a variable for the time the bot is launched
+gametimestarted = datetime.datetime.now()
+
+@bot.event
+async def on_ready():
+        print('Logged in as ')
+        print(bot.user.name)
+        print(bot.user.id)
+        print('Bot prefix is set to ' + prefix + '\n')
+        await bot.change_presence(game=discord.Game(name='with systemd'))
+
+@bot.command()
+async def uptime():
+        """Check bot uptime."""
+        global gametimestarted
+        await bot.say(timedelta_str(datetime.datetime.now() - gametimestarted))
+
+def timedelta_str(dt):
+        days = dt.days
+        hours, r = divmod(dt.seconds, 3600)
+        minutes, sec = divmod(r, 60)
+
+        if minutes == 1 and sec == 1:
+            return '{0} days, {1} hours, {2} minute and {3} second.'.format(days,hours,minutes,sec)
+        elif minutes > 1 and sec == 1:
+            return '{0} days, {1} hours, {2} minutes and {3} second.'.format(days,hours,minutes,sec)
+        elif minutes == 1 and sec > 1:
+            return '{0} days, {1} hours, {2} minute and {3} seconds.'.format(days,hours,minutes,sec)
+        else:
+            return '{0} days, {1} hours, {2} minutes and {3} seconds.'.format(days,hours,minutes,sec)
+
+bot.run(token)
